32e9e472f0eb14a102f010f8dfa94db1a86d195b
Leendert de Kok <koktaildotcom@hotmail.com>
2019-12-08 21:21:42 +0100

    transport full telegram

--- src/p1.js
+++ src/p1.js
@@ -14,7 +14,8 @@ logger.add(new logger.transports.Console({
  * 5: Refresh the page (you see couple of webpages being called)
  * 6: Copy the `homeyId` from the url: https://[localId].homey.homeylocal.com/api/manager/system/ping?id=[homeyId]
  */
-const homeyId = '[homeyId]'
+const homeyId1 = '5c46ead55c9fd81c1f3324a7'
+const homeyId2 = '5a7afd1f4445b17034a1e5c5'
 
 const usbPort = '/dev/ttyUSB0'
 const bps = 115200
@@ -22,67 +23,124 @@ const bits = 8
 const parity = 'none'
 
 const homeyEndpoint = '/update'
-const homeyHost = 'https://' + homeyId + '.connect.athom.com/api/app/com.p1'
+const homeyHost1 = 'https://' + homeyId1 + '.connect.athom.com/api/app/com.p1'
+const homeyHost2 = 'https://' + homeyId2 + '.connect.athom.com/api/app/com.p1'
 
-function publishToHomey (line) {
+function publishToHomey (output) {
     const data = {
-        'electricity': {
-            'received': {
-                'tariff2': {
-                    'reading': line['power'] &&
-                    line['power']['instantaneousProducedElectricityL2']
-                      ? line['power']['instantaneousProducedElectricityL2']
-                      : null,
-                    'unit': 'kWh',
+        "meterType": output.meterModel,
+        "equipmentId": output.power.equipmentId,
+        "version": output.dsmrVersion,
+        "timestamp": output.dsmrVersion,
+        "electricity": {
+            "received": {
+                "tariff1": {
+                    "reading": output.power.totalConsumed1,
+                    "unit": "kWh"
                 },
-                'actual': {
-                    'reading': line['power'] &&
-                    line['power']['instantaneousProducedElectricityL3']
-                      ? line['power']['instantaneousProducedElectricityL3']
-                      : null,
-                    'unit': 'kW',
+                "tariff2": {
+                    "reading": output.power.totalConsumed2,
+                    "unit": "kWh"
                 },
-                'tariff1': {
-                    'reading': line['power'] &&
-                    line['power']['instantaneousProducedElectricityL1']
-                      ? line['power']['instantaneousProducedElectricityL1']
-                      : null,
-                    'unit': 'kWh',
-                },
+                "actual": {
+                    "reading": output.power.totalConsumed,
+                    "unit": "kW"
+                }
             },
-            'delivered': {
-                'tariff2': {
-                    'reading': line['power'] &&
-                    line['power']['instantaneousConsumedElectricityL2']
-                      ? line['power']['instantaneousConsumedElectricityL2']
-                      : null,
-                    'unit': 'kWh',
+            "delivered": {
+                "tariff1": {
+                    "reading": output.power.totalProduced1,
+                    "unit": "kWh"
                 },
-                'actual': {
-                    'reading': line['power'] &&
-                    ['power']['instantaneousConsumedElectricityL3']
-                      ? line['power']['instantaneousConsumedElectricityL3']
-                      : null,
-                    'unit': 'kW',
+                "tariff2": {
+                    "reading": output.power.totalProduced2,
+                    "unit": "kWh"
                 },
-                'tariff1': {
-                    'reading': line['power'] &&
-                    line['power']['instantaneousConsumedElectricityL1']
-                      ? line['power']['instantaneousConsumedElectricityL1']
-                      : null,
-                    'unit': 'kWh',
-                },
+                "actual": {
+                    "reading": output.power.actualProduced,
+                    "unit": "kW"
+                }
             },
+            "tariffIndicator": output.power.activeTariff,
+            "switchPosition": output.power.switchPosition,
+            "voltageSags": {
+                "L1": output.power.voltageSagsL1,
+                "L2": output.power.voltageSagsL2,
+                "L3": output.power.voltageSagsL3
+            },
+            "voltageSwell": {
+                "L1": output.power.voltageSwellsL1,
+                "L2": output.power.voltageSwellsL2,
+                "L3": output.power.voltageSwellsL3
+            },
+            "instantaneous": {
+                "current": {
+                    "L1": {
+                        "reading": output.power.instantaneousCurrentL1,
+                        "unit": "A"
+                    },
+                    "L2": {
+                        "reading": output.power.instantaneousCurrentL2,
+                        "unit": "A"
+                    },
+                    "L3": {
+                        "reading": output.power.instantaneousCurrentL3,
+                        "unit": "A"
+                    }
+                },
+                "power": {
+                    "positive": {
+                        "L1": {
+                            "reading": output.power.instantaneousProducedElectricityL1,
+                            "unit": "kW"
+                        },
+                        "L2": {
+                            "reading": output.power.instantaneousProducedElectricityL2,
+                            "unit": "kW"
+                        },
+                        "L3": {
+                            "reading": output.power.instantaneousProducedElectricityL3,
+                            "unit": "kW"
+                        }
+                    },
+                    "negative": {
+                        "L1": {
+                            "reading": output.power.instantaneousProducedElectricityL1,
+                            "unit": "kW"
+                        },
+                        "L2": {
+                            "reading": output.power.instantaneousProducedElectricityL2,
+                            "unit": "kW"
+                        },
+                        "L3": {
+                            "reading": output.power.instantaneousProducedElectricityL3,
+                            "unit": "kW"
+                        }
+                    }
+                }
+            }
         },
-        'gas': {
-            'reading': line['gas'] && line['gas']['totalConsumed']
-              ? line['gas']['totalConsumed']
-              : null,
-            'unit': 'm3',
-        },
-    }
+        "gas": {
+            "deviceType": "003",
+            "equipmentId": output.gas.equipmentId,
+            "timestamp": output.gas.timestamp,
+            "reading": output.gas.totalConsumed,
+            "unit": "m3",
+            "valvePosition": output.gas.valvePosition
+        }
+    };
 
-    fetch(homeyHost + homeyEndpoint, {
+    fetch(homeyHost1 + homeyEndpoint, {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'},
+    }).then(() => {
+        // posted to homey!
+    }).catch((error) => {
+        logger.error(error)
+    })
+
+    fetch(homeyHost2 + homeyEndpoint, {
         method: 'post',
         body: JSON.stringify(data),
         headers: {'Content-Type': 'application/json'},
